5d0f49158329f6daae247cf7a3bc016222f6b5fb
laomd <965524991@qq.com>
2018-04-18 09:14:48 +0800

    HKLP端

---
+++ myutility/HKLPEnd.py
@@ -0,0 +1,8 @@
+class HKLPEnd(object):
+	def sendFile(self, target_socket, file):
+		"""send file to target socket, return true if send successfully or false."""
+		pass
+
+	def recvFile(self, src_socket, buffer_size):
+		"""receive file from source socket, return buffer received"""
+		pass
